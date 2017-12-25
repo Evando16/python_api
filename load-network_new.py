b9dea2d20c3d06b57baad7736e1a0ad6cb2e291f
ejunior <evando66.ea@gmail.com>
2017-12-25 18:47:57 -0200

    Nova configuração da rede TF

---
+++ load-network_new.py
@@ -0,0 +1,146 @@
+# from flask import Flask, request, Response, jsonify
+# from flask_json import FlaskJSON
+from openpyxl import load_workbook
+import tensorflow as tf
+# import json
+
+# app = Flask(__name__)
+# FlaskJSON(app)
+
+# @app.route('/', methods=['GET'])
+# def index():
+#     return 'Hello, Smoking!'
+
+# @app.route('/smoke/network/train', methods=['POST'])
+# def train():
+#     rangeTrain = 100000
+typeTrain = 'TREINAMENTO APOIO MEDIO'
+
+# if 'range' in request.args:
+#     rangeTrain = int(request.args['range'])
+
+# print('Range: '+ str(rangeTrain))
+
+# if 'typeTrain' in request.args:
+#     typeTrain = request.args['typeTrain']
+# else:
+#     resp = jsonify('Informe o tipo do treinamento')
+#     resp.status_code = 400
+#     return resp    
+
+# massas
+# TREINAMENTO- RESPOSTA A CARGA
+# TREINAMENTO APOIO TERMINAL
+# TREINAMENTO APOIO MEDIO
+# COMPARAO - XLS
+
+# read input data
+wb = load_workbook(filename='dados.xlsx', read_only=True)
+ws = wb[typeTrain]
+
+
+# read xlxs
+data = []
+index = 0
+for row in ws.rows:
+    data.append([])
+
+    for cell in row:
+        data[index].append(cell.value)
+    
+    index += 1
+
+#print(data[0])
+
+# output
+output = []
+for i in range(0, 21):
+    output.append([])
+    output[i].append(data[0][i])
+
+# ###print('output', output)
+# ###print('\n')
+
+# input
+input = []
+for i in range(1, len(data)):
+    for j in range(0, 1):
+        if i == 1:
+            input.append([])
+            input[j].append(1)
+
+        input[j].append(data[i][j])
+
+#print('input', input)
+
+# W = tf.Variable(tf.zeros([len(input[0]), 1]), tf.float32, name='W')
+# x = tf.placeholder(tf.float32, [None, len(input[0])], name='X')
+# y = tf.placeholder(tf.float32, [None, 1])
+
+# ###print('X: ', x)
+#print('W: ', W)
+
+# a = tf.sigmoid(tf.matmul(x, W), name='O')
+
+# #print(a)
+
+# loss = tf.reduce_mean(- (y * tf.log(a) + (1 - y) * tf.log(1 - a)))
+# train_step = tf.train.GradientDescentOptimizer(1e-5).minimize(loss)
+
+x_train = input
+y_train = output #[[0], [0], [1]]
+# sess = tf.InteractiveSession()
+# tf.global_variables_initializer().run()
+
+# saver = tf.train.Saver()
+
+# for epoch in range(rangeTrain):
+#     result = sess.run([train_step, loss, W], {x: x_train, y: y_train})
+
+#print('Final result:\nloss = ', result[1], '\nW = ', result[2])
+
+# print(sess.run(a, {x:x_train, y:y_train}))
+# print('-----------------------')
+
+# tf.train.write_graph(sess.graph_def, '.', 'smovetf.pbtxt') 
+# saver.save(sess, 'smovetf.ckpt')
+# saver.save(sess, './my_test_model',global_step=1000)
+
+#---
+
+sess=tf.Session()    
+
+#First let's load meta graph and restore weights
+saver = tf.train.import_meta_graph('my_model')
+# saver.restore(sess,tf.train.latest_checkpoint('./'))
+
+# Access saved Variables directly
+# print(sess.run('bias:0'))
+# This will print 2, which is the value of bias that we saved
+
+
+# Now, let's access and create placeholders variables and
+# create feed-dict to feed new data
+
+graph = tf.get_default_graph()
+w2 = graph.get_tensor_by_name("W:0")
+x1 = graph.get_tensor_by_name("X:0")
+y1 = graph.get_tensor_by_name("Y:0")
+
+
+#feed_dict = {w1:13.0, w2:17.0}
+
+#Now, access the op that you want to run. 
+op_to_restore = graph.get_tensor_by_name("smoke:0")
+
+print(sess.run(op_to_restore, {x1:x_train, y1:y_train } ))
+
+# print(sess.run([train_step, loss, W], {x: x_train, y: y_train}))
+# print sess.run(op_to_restore,feed_dict)
+#This will print 60 which is calculated 
+#---
+
+# resp = jsonify((sess.run(a, {x:x_train, y:y_train}).tolist()))
+# resp.status_code = 200
+# return json.dumps(sess.run(op_to_restore, {x1:y_train, x1:x_train}).tolist())
+#return 'ok'
---
+++ smoke_app_new.py
@@ -0,0 +1,112 @@
+# from flask import Flask, request, Response, jsonify
+# from flask_json import FlaskJSON
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+from openpyxl import load_workbook
+import tensorflow as tf
+# import json
+
+# app = Flask(__name__)
+# FlaskJSON(app)
+
+# @app.route('/', methods=['GET'])
+# def index():
+#     return 'Hello, Smoking!'
+
+# @app.route('/smoke/network/train', methods=['POST'])
+# def train():
+rangeTrain = 10    
+typeTrain = 'TREINAMENTO APOIO MEDIO'
+
+# if 'range' in request.args:
+#     rangeTrain = int(request.args['range'])
+
+# print('Range: '+ str(rangeTrain))
+
+# if 'typeTrain' in request.args:
+#     typeTrain = request.args['typeTrain']
+# else:
+#     return json.dumps('Informe o tipo do treinamento')  
+
+# massas
+# TREINAMENTO- RESPOSTA A CARGA
+# TREINAMENTO APOIO TERMINAL
+# TREINAMENTO APOIO MEDIO
+# COMPARAO - XLS
+
+# read input data
+wb = load_workbook(filename='dados.xlsx', read_only=True)
+ws = wb[typeTrain]
+
+
+# read xlxs
+data = []
+index = 0
+for row in ws.rows:
+    data.append([])
+
+    for cell in row:
+        data[index].append(cell.value)
+    
+    index += 1
+
+#print(data[0])
+
+# output
+output = []
+for i in range(0, len(data[0])):
+    output.append([])
+    output[i].append(data[0][i])
+
+# ###print('output', output)
+# ###print('\n')
+
+# input
+input = []
+
+for i in range(1, len(data)):
+    for j in range(0, 1):
+        if i == 1:
+            input.append([])
+            input[j].append(1)
+
+        input[j].append(data[i][j])
+
+#print('input', input)
+
+W = tf.Variable(tf.zeros([len(input[0]), 1]), tf.float32, name='W')
+x = tf.placeholder(tf.float32, [None, len(input[0])], name='X')
+y = tf.placeholder(tf.float32, [None, 1], name='Y')
+
+# ###print('X: ', x)
+#print('W: ', W)
+
+a = tf.sigmoid(tf.matmul(x, W), name='smoke')
+
+#print(a)
+
+loss = tf.reduce_mean(- (y * tf.log(a) + (1 - y) * tf.log(1 - a)))
+train_step = tf.train.GradientDescentOptimizer(1e-5).minimize(loss)
+
+x_train = input #[1, 34.62, 78.02]
+y_train = output #[[0], [0], [1]]
+# print(input[0])
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+saver = tf.train.Saver()
+
+for epoch in range(rangeTrain):
+    result = sess.run([train_step, loss, W], {x: x_train, y: y_train})
+
+saver.export_meta_graph('my_model')
+
+#print('Final result:\nloss = ', result[1], '\nW = ', result[2])
+
+print(sess.run(a, {x:x_train, y:y_train}))    
+
+# tf.train.write_graph(sess.graph_def, '.', 'smovetf.pbtxt') 
+# saver.save(sess, 'smovetf.ckpt')
+
+# return json.dumps(sess.run(a, {x:x_train, y:y_train}).tolist())
